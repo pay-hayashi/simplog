ef0e9887c66890fd5902398e48e3a9f2d888daf4
pay-hayashi <junlaurantlaurant@gmail.com>
2023-02-19 17:21:46 +0900

    add slash to base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://pay-hayashi.github.io',
-  base: 'simplog'
+  base: '/simplog'
 });
